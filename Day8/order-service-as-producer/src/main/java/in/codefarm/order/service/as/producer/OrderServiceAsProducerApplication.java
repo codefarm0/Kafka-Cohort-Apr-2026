bd6168532ac08df9db0a601941e9a19a0702b84c
Codefarm <whytechnology01@gmail.com>
2026-04-16 22:11:52 +0530

    day 8 code

---
+++ Day8/order-service-as-producer/src/main/java/in/codefarm/order/service/as/producer/OrderServiceAsProducerApplication.java
@@ -0,0 +1,17 @@
+package in.codefarm.order.service.as.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.EnumMap;
+
+@SpringBootApplication
+public class OrderServiceAsProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceAsProducerApplication.class, args);
+
+		EnumMap
+	}
+
+}
